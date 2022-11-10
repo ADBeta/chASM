6057976566287e2eec819f7254bf64d3e72b6787
Ashley <ashleydylanrudd@gmail.com>
2022-11-10 20:54:34 +0000

    V3.1.4
    Improved speed by removing bitwise inversions per call.

--- src/chASM.cpp
+++ src/chASM.cpp
@@ -3,37 +3,39 @@
 //Initialise the assembly pin object and set all the internal vars
 chASM::chASM(uint8_t pin) {
 	//Set the internal variables using the builtin PROGMEM macros
-	this->_asm_mask = digitalPinToBitMask(pin); //Bit mask in port byte (PGM)
-	this->_asm_port = digitalPinToPort(pin); //Port byte (PGM)
+	_asm_mask_nom = digitalPinToBitMask(pin); //Bitmask of chASM Pin (PGM)
+	_asm_mask_inv = ~_asm_mask_nom; //Invert the Bitmask to use during exec
+		
+	_asm_port = digitalPinToPort(pin); //Port byte (PGM)
 	
-	this->_asm_ddr = portModeRegister(_asm_port); //Data Direction (volatile)
-	this->_asm_opr = portOutputRegister(_asm_port); //Ouput Register (volatile)
-	this->_asm_ipr = portInputRegister(_asm_port); //Input Register (volatile)
+	_asm_ddr = portModeRegister(_asm_port); //Data Direction (volatile)
+	_asm_opr = portOutputRegister(_asm_port); //Ouput Register (volatile)
+	_asm_ipr = portInputRegister(_asm_port); //Input Register (volatile)
 }
 
 //Set the mode register of the pin
 void chASM::setMode(uint8_t mode) {
 	if(mode == OUTPUT) {
-		*_asm_ddr |= _asm_mask; //DDR bit to 1
+		*_asm_ddr |= _asm_mask_nom; //DDR bit to 1
 	}
 	
 	if(mode == INPUT) { 
-		*_asm_ddr &= ~_asm_mask; //DDR bit to 0
-		*_asm_opr &= ~_asm_mask; //Output register to 0
+		*_asm_ddr &= _asm_mask_inv; //DDR bit to 0
+		*_asm_opr &= _asm_mask_inv; //Output register to 0
 	}
 	
 	if(mode == INPUT_PULLUP) {
-		*_asm_ddr &= ~_asm_mask; //DDR bit to 0
-		*_asm_opr |= _asm_mask; //Output register to 1
+		*_asm_ddr &= _asm_mask_inv; //DDR bit to 0
+		*_asm_opr |= _asm_mask_nom; //Output register to 1
 	}
 }
 
 //Write binary state to the pin
 void chASM::write(bool state) {
-	if (state == 0) {
-		*_asm_opr &= ~_asm_mask; //Set mask bit in output register to LOW
+	if(state == 0) {
+		*_asm_opr &= _asm_mask_inv; //Set mask bit in output register to LOW
 	} else {
-		*_asm_opr |= _asm_mask; //Set mask bit in output register to HIGH
+		*_asm_opr |= _asm_mask_nom; //Set mask bit in output register to HIGH
 	}
 }
 
@@ -41,7 +43,7 @@ void chASM::write(bool state) {
 bool chASM::read(void) {
 	//Read the input register and mask it. Simple comparation is faster than	
 	//greater than/less than etc. If compare with 0 is true, the pin is LOW
-	if((*_asm_ipr & _asm_mask) == 0) {
+	if((*_asm_ipr & _asm_mask_nom) == 0) {
 		return 0;
 	}
 	
--- src/chASM.h
+++ src/chASM.h
@@ -10,6 +10,7 @@
 	//TODO Add usage to readme
 	//TODO Add timer control? (at least kill timer?)
 	
+	Version 3.1.4
 	Last Modified 10 Nov 2022
 	(c) ADBeta
 */
@@ -38,13 +39,16 @@ class chASM {
 	void setMode(uint8_t mode);
 	
 	private:
-	uint8_t _asm_mask; //Bitmask - the pin is x bit in the byte
+	/* Note: Removing the bitwise inversion during exec is faster, so I define
+	variables for pre-computing those numbers */
+	uint8_t _asm_mask_nom; //Bitmask of the chASM pin - Normal form, eg 00010
+	uint8_t _asm_mask_inv; //Bitmask inverted eg 11101
+	
 	uint8_t _asm_port; //Which hardware port the pin belongs to
 	
 	volatile uint8_t *_asm_ddr; //Data Direction Register (for input/output)
 	volatile uint8_t *_asm_opr; //Output register for the pin
 	volatile uint8_t *_asm_ipr; //Input register for the pin
-
 }; //class chASM
 
 #endif
